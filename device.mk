d990f7f87aad04098ee672d9cdebb38460ad5869
Woomymy <woomy@woomy.be>
2023-02-27 15:31:40 +0100

    X6882: Add partitions configuration
    Signed-off-by: Woomymy <woomy@woomy.be>
    Change-Id: I9e08db2236376b1132aa3bd8a7abf19aa70ba48c
    Signed-off-by: dodyirawan85 <40514988+dodyirawan85@users.noreply.github.com>
    Signed-off-by: fjrXTR <fajarslebew31@gmail.com>

--- device.mk
+++ device.mk
@@ -7,6 +7,9 @@
 # API levels
 PRODUCT_SHIPPING_API_LEVEL := 33
 
+# Dynamic partitions
+PRODUCT_USE_DYNAMIC_PARTITIONS := true
+
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += \
     $(LOCAL_PATH)
