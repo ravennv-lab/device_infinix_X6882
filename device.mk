2a7140b72f2eac4ca8e1e603d62e27bf69d0a391
Woomymy <woomy@woomy.be>
2023-03-03 11:48:40 +0100

    X6882: Install developer gsi keys
    Signed-off-by: Woomymy <woomy@woomy.be>
    Change-Id: I3d4a330788e32d8ba967420c602436f882a26b92
    Signed-off-by: dodyirawan85 <40514988+dodyirawan85@users.noreply.github.com>
    Signed-off-by: fjrXTR <fajarslebew31@gmail.com>

--- device.mk
+++ device.mk
@@ -67,6 +67,9 @@ PRODUCT_PACKAGES += \
 # Shipping API level
 PRODUCT_SHIPPING_API_LEVEL := 33
 
+# Include GSI keys
+$(call inherit-product, $(SRC_TARGET_DIR)/product/developer_gsi_keys.mk)
+
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += \
     $(LOCAL_PATH) \
