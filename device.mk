049d0d6e9953fb5b66a8d4254fe9fd0fbed66b01
Michael Bestas <mkbestas@lineageos.org>
2024-07-22 00:03:18 +0100

    X6882: overlay: Enable battery cycle count
    Change-Id: I92bc8363e1c9e2ad54c29a0a30dce992e25f697d
    Signed-off-by: fjrXTR <fajarslebew31@gmail.com>

--- device.mk
+++ device.mk
@@ -181,7 +181,8 @@ PRODUCT_PACKAGES += \
 # Lineage-Specific Overlays
 PRODUCT_PACKAGES += \
 	LineageApertureOverlayTarget \
-    LineageDialerOverlayTarget
+    LineageDialerOverlayTarget \
+    LineageSettingsOverlayTarget
 
 # Linker config
 PRODUCT_VENDOR_LINKER_CONFIG_FRAGMENTS += \
@@ -245,7 +246,8 @@ PRODUCT_PACKAGES += \
 
 PRODUCT_PACKAGES += \
     LineageApertureOverlayX6739 \
-    LineageDialerOverlayX6739
+    LineageDialerOverlayX6739 \
+    LineageSettingsOverlayX6739
 
 # Permissions
 PRODUCT_COPY_FILES += \
