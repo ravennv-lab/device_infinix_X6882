e8c49ec5363506c2566f23b51176d823c224c1ca
xyzuniverse <dev.xyzuniverse@gmail.com>
2025-05-08 04:42:41 +0000

    X6882: Silence C2MtkBufferManager logspam
    Signed-off-by: xyzuniverse <dev.xyzuniverse@gmail.com>
    Signed-off-by: fjrXTR <fajarslebew31@gmail.com>
    Signed-off-by: hansnvv <hanssnnv@gmail.com>

--- vendor_logtag.mk
+++ vendor_logtag.mk
@@ -275,4 +275,5 @@ PRODUCT_VENDOR_PROPERTIES += \
     persist.log.tag.wlan_assistant=$(VENDOR_LOG_LEVEL) \
     persist.log.tag.netd=$(VENDOR_LOG_LEVEL) \
     persist.log.tag.HWUI=$(VENDOR_LOG_LEVEL) \
-    persist.log.tag.FrameTracker=$(VENDOR_LOG_LEVEL)
+    persist.log.tag.FrameTracker=$(VENDOR_LOG_LEVEL) \
+    persist.log.tag.C2MtkBufferManager=$(VENDOR_LOG_LEVEL)
