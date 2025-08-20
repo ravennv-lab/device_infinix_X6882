92073b73c17a063414c94263124e0fa2c8af139d
Shirayuki39 <lorddemecrius83@proton.me>
2025-08-20 14:47:15 +0900

    X6882: Silence lights_service_transsion_rust logging
    * We already know our rust rewrite of lights-service.transsion works fine, we don't need the `Lights setting state for id=0 to color` logspam
    
    Change-Id: I1da4192f6b7890aaef13439c881836c1c5f3cff2
    Signed-off-by: Shirayuki39 <lorddemecrius83@proton.me>
    Signed-off-by: fjrXTR <fajarslebew31@gmail.com>
    Signed-off-by: hansnvv <hanssnnv@gmail.com>

--- vendor_logtag.mk
+++ vendor_logtag.mk
@@ -278,4 +278,5 @@ PRODUCT_VENDOR_PROPERTIES += \
     persist.log.tag.FrameTracker=$(VENDOR_LOG_LEVEL) \
     persist.log.tag.C2MtkBufferManager=$(VENDOR_LOG_LEVEL) \
     persist.log.tag.android.hardware.graphics.composer@2.3-service=$(VENDOR_LOG_LEVEL) \
-    persist.log.tag.surfaceflinger$(VENDOR_LOG_LEVEL)
+    persist.log.tag.surfaceflinger$(VENDOR_LOG_LEVEL) \
+    persist.log.tag.lights_service_transsion_rust=$(VENDOR_LOG_LEVEL)
