2529c400b8b916d44cc77cbdd9591197f20497ac
hansnvv <hanssnnv@gmail.com>
2026-06-06 17:46:55 +0700

    X6882: Import Gatekeeper stack

--- device.mk
+++ device.mk
@@ -162,6 +162,11 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.biometrics.fingerprint@2.1.vendor:64
 
+# Gatekeeper
+PRODUCT_PACKAGES += \
+    android.hardware.gatekeeper@1.0-impl \
+    android.hardware.gatekeeper@1.0-service
+
 # Health
 PRODUCT_PACKAGES += \
     android.hardware.health@2.1-service \
